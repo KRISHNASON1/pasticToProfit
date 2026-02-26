386c8233371677621a286c518451d7fc72022146
rajatbhardwaz <rajatbhardwaj1804@gmail.com>
2026-02-27 03:26:11 +0530

    marketplace update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Sidebar from './components/Sidebar/Sidebar';
 import Cart from './components/Cart/Cart';
@@ -9,25 +9,51 @@ import Support from './pages/Support/Support';
 import Settings from './pages/Settings/Settings';
 import './App.css';
 
+function AppLayout() {
+  const location = useLocation();
+  const isMarketplace = location.pathname === '/marketplace';
+
+  // Marketplace gets its own full-screen layout without sidebar
+  if (isMarketplace) {
+    return (
+      <div className="app-layout app-fullscreen">
+        <Marketplace />
+        <Cart />
+      </div>
+    );
+  }
+
+  return (
+    <div className="app-layout">
+      <Sidebar />
+      <main className="main-content">
+        <div className="main-inner">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/rewards" element={<Rewards />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </main>
+      <Cart />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <CartProvider>
-        <div className="app-layout">
-          <Sidebar />
-          <main className="main-content">
-            <div className="main-inner">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/marketplace" element={<Marketplace />} />
-                <Route path="/rewards" element={<Rewards />} />
-                <Route path="/support" element={<Support />} />
-                <Route path="/settings" element={<Settings />} />
-              </Routes>
+        <Routes>
+          <Route path="/marketplace" element={
+            <div className="app-layout app-fullscreen">
+              <Marketplace />
+              <Cart />
             </div>
-          </main>
-          <Cart />
-        </div>
+          } />
+          <Route path="*" element={<AppLayout />} />
+        </Routes>
       </CartProvider>
     </BrowserRouter>
   );

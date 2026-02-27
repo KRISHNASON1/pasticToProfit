475d78d41a815f0ee46a1efdcf0a53e6477070b8
Krishna Soni <krishna191105@gmail.com>
2026-02-27 13:22:09 +0530

    fine tuning

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['pastictoprofit.onrender.com'],
+    allowedHosts: ['upcycle.onrender.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
